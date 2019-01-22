ad46a3a32002f4a3b8d08952db77fbeba5031a03
NawabMahmood <45463346+NawabMahmood@users.noreply.github.com>
2019-01-22 17:02:31 -0500

    Add files via upload
    first big project

---
+++ practice_racetrack.ino
@@ -0,0 +1,55 @@
+#include <Servo.h>;
+#include <LiquidCrystal.h>;
+
+LiquidCrystal lcd(12,11,4,3,2);
+
+
+Servo myservo;
+const int switchs = 6;
+int timer = 0;
+bool racestarted=false;
+
+void setup() {
+  lcd.begin(16,2);
+//  tells how big window is for words;
+  myservo.attach(9);
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+  lcd.print("time is:");
+
+
+}
+
+void loop() {
+
+
+int switchstate = digitalRead(switchs);
+//Serial.println(switchstate);
+if(switchstate == 1){
+  racestarted=true;
+ 
+  myservo.write(0);
+   
+  
+}
+else
+{ myservo.write(100);}
+
+
+
+    
+    
+    
+    if(racestarted == true){
+    
+        timer++;
+        Serial.println(timer);
+        lcd.setCursor(0,1);
+        lcd.print(timer);
+        delay(1000);
+        
+    }
+}
+
+  
+
