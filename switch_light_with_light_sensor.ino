e57d0f1290be7426e3acd63e88652ee3aca2907b
NawabMahmood <45463346+NawabMahmood@users.noreply.github.com>
2019-01-22 17:01:29 -0500

    Add files via upload

---
+++ switch_light_with_light_sensor.ino
@@ -0,0 +1,26 @@
+void setup() {
+ Serial.begin(9600);
+ pinMode(7,INPUT);
+ pinMode(9,OUTPUT);
+
+}
+
+void loop() {
+  int a = analogRead(A1);
+  int S =  digitalRead(7);
+
+  if( S == 1){
+
+    if(a<10){
+      digitalWrite(9,1);
+    }
+    else{ 
+      digitalWrite(9,0);
+    }
+    
+  }
+
+else{ 
+  digitalWrite(9,0);
+  }
+}
